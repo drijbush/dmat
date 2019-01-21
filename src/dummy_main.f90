7028330d45f18e9039cef4760bd5f868cce22578
Ian Bush <Ian.Bush@oerc.ox.ac.uk>
2019-01-21 11:15:17 +0000

    Setting up initial structure

---
+++ src/dummy_main.f90
@@ -0,0 +1,7 @@
+Program dummy_main
+
+  Use distributed_matrices_module
+
+  Implicit None
+  
+End Program dummy_main
---
+++ src/matrix.f90
@@ -0,0 +1,10 @@
+
+Module distributed_matrices_module
+
+  Use numbers_module, Only : wp
+  
+  Implicit None
+
+  Private
+
+End Module distributed_matrices_module
