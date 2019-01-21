99822006b9d59e404baa57dad974fcc1d56214ef
Ian Bush <Ian.Bush@oerc.ox.ac.uk>
2019-01-21 11:57:44 +0000

    Added real matrix

--- src/matrix.f90
+++ src/matrix.f90
@@ -4,10 +4,18 @@ Module distributed_matrix_module
   
   Implicit None
 
-  Type, Abstract :: distributed_matrix_ops
+  Type, Abstract, Public :: distributed_matrix_ops
+     Integer, Dimension( : ), Allocatable :: descriptor
+     Integer                              :: communicator
    Contains
      Procedure( diag_interface ), Deferred :: diag
   End Type distributed_matrix_ops
+
+  Type, Extends( distributed_matrix_ops ), Public :: real_distributed_matrix
+     Real( wp ), Dimension( : ), Allocatable :: data
+   Contains
+     Procedure :: diag => real_distributed_matrix_diag
+  End type real_distributed_matrix
 
   Private  
 
@@ -21,5 +29,14 @@ Module distributed_matrix_module
        Real( wp ), Dimension( : )     , Intent(   Out ) :: E
      End Subroutine diag_interface
   End Interface
+
+Contains
+
+  Subroutine real_distributed_matrix_diag( A, Q, E )
+    Implicit None
+    Class( real_distributed_matrix ), Intent( In    ) :: A
+    Class( distributed_matrix_ops  ), Intent(   Out ) :: Q
+    Real( wp ), Dimension( : )      , Intent(   Out ) :: E
+  End Subroutine real_distributed_matrix_diag
   
 End Module distributed_matrix_module
