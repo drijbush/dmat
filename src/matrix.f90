117cad9c317f6b7b1fac903728ddb5149126b010
Ian Bush <Ian.Bush@oerc.ox.ac.uk>
2019-01-22 10:30:07 +0000

    Added allocations so now compiles without warnings

--- src/matrix.f90
+++ src/matrix.f90
@@ -5,23 +5,23 @@ Module distributed_matrix_module
   Implicit None
 
   Type, Abstract, Public :: distributed_matrix_ops
-     Integer, Dimension( : ), Allocatable :: descriptor
-     Integer                              :: communicator
+     Integer, Dimension( : ), Allocatable, Private :: descriptor
+     Integer                             , Private :: communicator
    Contains
      Procedure( diag_interface ), Deferred :: diag
   End Type distributed_matrix_ops
 
   Type, Extends( distributed_matrix_ops ), Public :: real_distributed_matrix
-     Real( wp ), Dimension( : ), Allocatable :: data
+     Real( wp ), Dimension( : ), Allocatable, Private :: data
    Contains
-     Procedure :: diag => real_distributed_matrix_diag
-  End type real_distributed_matrix
+     Procedure  :: diag => real_distributed_matrix_diag
+  End Type real_distributed_matrix
 
   Type, Extends( distributed_matrix_ops ), Public :: complex_distributed_matrix
-     Complex( wp ), Dimension( : ), Allocatable :: data
+     Complex( wp ), Dimension( : ), Allocatable, Private :: data
    Contains
      Procedure :: diag => complex_distributed_matrix_diag
-  End type complex_distributed_matrix
+  End Type complex_distributed_matrix
 
   Private  
 
@@ -30,26 +30,56 @@ Module distributed_matrix_module
        Import :: distributed_matrix_ops
        Import :: wp
        Implicit None
-       Class( distributed_matrix_ops ), Intent( In    ) :: A
-       Class( distributed_matrix_ops ), Intent(   Out ) :: Q
-       Real( wp ), Dimension( : )     , Intent(   Out ) :: E
+       Class( distributed_matrix_ops ),              Intent( In    ) :: A
+       Class( distributed_matrix_ops ), Allocatable, Intent(   Out ) :: Q
+       Real( wp ), Dimension( : )     , Allocatable, Intent(   Out ) :: E
      End Subroutine diag_interface
   End Interface
+
+  Integer, Parameter :: desc_n = 1
 
 Contains
 
   Subroutine real_distributed_matrix_diag( A, Q, E )
+
+    Use numbers_module, Only : wp
+
     Implicit None
-    Class( real_distributed_matrix ), Intent( In    ) :: A
-    Class( distributed_matrix_ops  ), Intent(   Out ) :: Q
-    Real( wp ), Dimension( : )      , Intent(   Out ) :: E
+
+    Class( real_distributed_matrix ),              Intent( In    ) :: A
+    Class( distributed_matrix_ops  ), Allocatable, Intent(   Out ) :: Q
+    Real( wp ), Dimension( : )      , Allocatable, Intent(   Out ) :: E
+
+    Integer :: n
+
+    Allocate( Q, Source = A )
+
+    ! Want to change to function geting things from descriptor eventually
+    n = A%descriptor( desc_n )
+    
+    Allocate( E( 1:n ) )
+
   End Subroutine real_distributed_matrix_diag
   
   Subroutine complex_distributed_matrix_diag( A, Q, E )
+
+    Use numbers_module, Only : wp
+    
     Implicit None
-    Class( complex_distributed_matrix ), Intent( In    ) :: A
-    Class( distributed_matrix_ops     ), Intent(   Out ) :: Q
-    Real( wp ), Dimension( : )         , Intent(   Out ) :: E
+
+    Class( complex_distributed_matrix ),              Intent( In    ) :: A
+    Class( distributed_matrix_ops     ), Allocatable, Intent(   Out ) :: Q
+    Real( wp ), Dimension( : )         , Allocatable, Intent(   Out ) :: E
+
+    Integer :: n
+
+    Allocate( Q, Source = A )
+
+    ! Want to change to function geting things from descriptor eventually
+    n = A%descriptor( desc_n )
+    
+    Allocate( E( 1:n ) )
+
   End Subroutine complex_distributed_matrix_diag
   
 End Module distributed_matrix_module
