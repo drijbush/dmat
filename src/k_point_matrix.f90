7865b1b417e68540295c613f93a20290e85c15a4
Ian Bush <Ian.Bush@oerc.ox.ac.uk>
2019-02-26 14:28:49 +0000

    Added replciation of evals code

--- src/k_point_matrix.f90
+++ src/k_point_matrix.f90
@@ -10,6 +10,7 @@ Module k_point_matrix_module
   Integer, Public, Parameter :: K_POINT_COMPLEX = 1
 
   Integer, Private, Parameter :: INVALID = -1
+  Integer, Private, Parameter :: NOT_ME  = -2
 
   Type k_point_info
      Integer                              :: spin
@@ -23,8 +24,6 @@ Module k_point_matrix_module
   End type k_point_matrices
 
   Type, Private :: k_point
-!!$     Integer                 , Dimension( : ), Allocatable :: spin
-!!$     Integer                 , Dimension( : ), Allocatable :: k
      Type( k_point_info     )                              :: info
      ! This splitting allows irreps
      Type( k_point_matrices ), Dimension( : ), Allocatable :: data
@@ -32,7 +31,7 @@ Module k_point_matrix_module
      Integer                                               :: communicator = INVALID
   End type k_point
 
-  Type, Public :: distributed_k_matrices
+  Type, Public :: ks_array
      Type( k_point_info ), Dimension( : ), Allocatable :: all_k_point_info
      ! this splitting allows multiple k points on this process
      Type( k_point      ), Dimension( : ), Allocatable :: my_k_points
@@ -41,8 +40,9 @@ Module k_point_matrix_module
    Contains
      Procedure :: create => matrices_create
      Procedure :: diag   => matrices_diag
-     Procedure, Private :: get_sk_index
-  End type distributed_k_matrices
+     Procedure, Private :: get_all_ks_index
+     Procedure, Private :: get_my_ks_index
+  End type ks_array
   
   Type, Public :: eval_storage
      Real( wp ), Dimension( : ), Allocatable :: evals
@@ -78,7 +78,7 @@ Contains
     
     ! Also want to think about what kind of object should be used as a source
     
-    Class( distributed_k_matrices ), Intent(   Out ) :: matrices
+    Class( ks_array )              , Intent(   Out ) :: matrices
     Integer                        , Intent( In    ) :: n_spin
     Integer, Dimension( :    )     , Intent( In    ) :: k_point_type
     Integer, Dimension( :, : )     , Intent( In    ) :: k_points
@@ -87,34 +87,34 @@ Contains
     Type ( distributed_k_matrix   ), Intent( In    ) :: source
 
     Integer :: n_k_points
-    Integer :: s, k, sk
+    Integer :: s, k, ks
     
     n_k_points = Size( k_point_type)
 
     ! Set up the all k point data structure
     Allocate( matrices%all_k_point_info( 1:n_spin * n_k_points ) )
-    sk = 0
+    ks = 0
     Do s = 1, n_spin
        Do k = 1, n_k_points
-          sk = sk + 1
-          matrices%all_k_point_info( sk )%spin      = s
-          matrices%all_k_point_info( sk )%k_indices = k_points( :, k )
+          ks = ks + 1
+          matrices%all_k_point_info( ks )%spin      = s
+          matrices%all_k_point_info( ks )%k_indices = k_points( :, k )
        End Do
     End Do
 
     ! Now my k points
     Allocate( matrices%my_k_points( 1:n_spin * n_k_points ) )
-    sk = 0
+    ks = 0
     Do s = 1, n_spin
        Do k = 1, n_k_points
-          sk = sk + 1
-          matrices%my_k_points( sk )%info%spin    = s
-          matrices%my_k_points( sk )%info%k_indices = k_points( :, k )
-          Allocate( matrices%my_k_points( sk )%data( 1:1 ) )
-          matrices%my_k_points( sk )%data( 1 )%label = 1
-          Call matrices%my_k_points( sk )%data( 1 )%matrix%create( k_point_type( k ) == K_POINT_COMPLEX, &
+          ks = ks + 1
+          matrices%my_k_points( ks )%info%spin    = s
+          matrices%my_k_points( ks )%info%k_indices = k_points( :, k )
+          Allocate( matrices%my_k_points( ks )%data( 1:1 ) )
+          matrices%my_k_points( ks )%data( 1 )%label = 1
+          Call matrices%my_k_points( ks )%data( 1 )%matrix%create( k_point_type( k ) == K_POINT_COMPLEX, &
                s, k_points( :, k ), m, n, source )
-          matrices%my_k_points( sk )%communicator = source%get_comm()
+          matrices%my_k_points( ks )%communicator = source%get_comm()
        End Do
     End Do
 
@@ -124,42 +124,108 @@ Contains
 
   Subroutine matrices_diag( A, Q, E )
 
-    Class( distributed_k_matrices ),                 Intent( In    ) :: A
-    Type ( distributed_k_matrices ),                 Intent(   Out ) :: Q
-    Type ( eval_storage           ), Dimension( : ), Intent(   Out ) :: E
+    Use mpi
 
+    Class( ks_array     ),                 Intent( In    ) :: A
+    Type ( ks_array     ),                 Intent(   Out ) :: Q
+    Type ( eval_storage ), Dimension( : ), Intent(   Out ) :: E
+
+    Real( wp ) :: rdum
+
+    Integer, Dimension( 1:2 ) :: buff_send, buff_recv
+    
+    Integer :: me, me_parent
+    Integer :: ks_root, nb
+    Integer :: error
+    Integer :: request
+    Integer :: rsize, handle
     Integer :: my_ks, ks
 
+    Logical :: sending_data
+    
     ! Make Q have the same set up as A
     Q = A
 
     Do my_ks = 1, Size( A%my_k_points )
-       ! Probably want to combin ks and make evals a 1D array
-       ks = A%get_sk_index( my_ks )
-       If( ks /= INVALID ) Then
-          Call A%my_k_points( my_ks )%data( 1 )%matrix%diag( Q%my_k_points( my_ks )%data( 1 )%matrix, E( ks )%evals )
-       End If
+       ks = A%get_all_ks_index( my_ks )
+       Call A%my_k_points( my_ks )%data( 1 )%matrix%diag( Q%my_k_points( my_ks )%data( 1 )%matrix, E( ks )%evals )
     End Do
 
     ! Replicate evals
+    Call mpi_comm_rank( A%parent_communicator, me_parent, error )
+    Do ks = 1, Size( A%all_k_point_info )
+       my_ks = A%get_my_ks_index( ks )
+       ! Work out who holds this set of evals and send how many there are
+       ! the root node of the communicator holding them back to the root node of the parent communicator
+       sending_data = .False.
+       If( my_ks /= NOT_ME ) Then
+          Call mpi_comm_rank( A%my_k_points( my_ks )%communicator, me, error )
+          sending_data = me == 0
+          If( sending_data ) then
+             buff_send( 1 ) = me
+             buff_send( 2 ) = Size( E( ks )%evals )
+             Call mpi_isend( buff_send, 2, MPI_INTEGER, 0, 10, A%parent_communicator, request, error )
+          End If
+       End If
+       If( me_parent == 0 ) Then
+          Call mpi_recv( buff_recv, 2, MPI_INTEGER, MPI_ANY_SOURCE, 10, A%parent_communicator, MPI_STATUS_IGNORE, error )
+       End If
+       If( sending_data ) Then
+          Call mpi_wait( request, MPI_STATUS_IGNORE, error )
+       End If
+       ! Now on root of parent bcast back to all
+       Call mpi_bcast( buff_recv, 2, MPI_INTEGER, 0, A%parent_communicator, error )
+       ks_root = buff_recv( 1 )
+       nb      = buff_recv( 2 )
+       ! Now know how many evals we will recv - allocate memory if haven't done so already
+       If( .Not. Allocated( E( ks )%evals ) ) Then
+          Allocate( E( ks )%evals( 1:nb ) )
+       End If
+       ! And finally bcast out the values from the root node for this set of evals
+       Call mpi_sizeof( rdum, rsize, error )
+       Call mpi_type_match_size( MPI_TYPECLASS_REAL, rsize, handle, error )
+       Call mpi_bcast( E( ks )%evals, nb, handle, ks_root, A%parent_communicator, error )          
+    End Do
     
   End Subroutine matrices_diag
 
-  Pure Function get_sk_index( A, my_sk ) Result( sk )
+  Pure Function get_all_ks_index( A, my_ks ) Result( ks )
 
-    Integer :: sk
+    Integer :: ks
 
-    Class( distributed_k_matrices ), Intent( In ) :: A
-    Integer                        , Intent( In ) :: my_sk
+    Class( ks_array ), Intent( In ) :: A
+    Integer          , Intent( In ) :: my_ks
 
-    Do sk = 1, Size( A%all_k_point_info )
-       If( A%all_k_point_info( sk )%spin == A%my_k_points( my_sk )%info%spin ) Then
-          If( All( A%all_k_point_info( sk )%k_indices == A%my_k_points( my_sk )%info%k_indices ) ) Then
+    Do ks = 1, Size( A%all_k_point_info )
+       If( A%all_k_point_info( ks )%spin == A%my_k_points( my_ks )%info%spin ) Then
+          If( All( A%all_k_point_info( ks )%k_indices == A%my_k_points( my_ks )%info%k_indices ) ) Then
              Exit
           End If
        End If
     End Do
     
-  End Function get_sk_index
+  End Function get_all_ks_index
+
+  Pure Function get_my_ks_index( A, ks ) Result( my_ks )
+
+    Integer :: my_ks
+
+    Class( ks_array ), Intent( In ) :: A
+    Integer          , Intent( In ) :: ks
+
+    Integer :: k
+    
+    my_ks = NOT_ME
+
+    Do k = 1, Size( A%my_k_points )
+       If( A%all_k_point_info( ks )%spin == A%my_k_points( k )%info%spin ) Then
+          If( All( A%all_k_point_info( ks )%k_indices == A%my_k_points( k )%info%k_indices ) ) Then
+             my_ks = k
+             Exit
+          End If
+       End If
+    End Do
+
+  End Function get_my_ks_index
 
 End Module k_point_matrix_module
